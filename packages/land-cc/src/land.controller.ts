7b334c399d72d7b5851503e06be3df57eb982300
Ravindu Sachintha <rs@ip-172-31-23-237.ap-southeast-1.compute.internal>
2020-01-31 07:35:03 +0000

    chain code updated with basic functionalities

--- packages/land-cc/src/land.controller.ts
+++ packages/land-cc/src/land.controller.ts
@@ -1,20 +1,192 @@
+import * as yup from 'yup';
 import { ChaincodeTx } from '@worldsibu/convector-platform-fabric';
 import {
   Controller,
   ConvectorController,
   Invokable,
-  Param
+  Param,
+  History
 } from '@worldsibu/convector-core';
 
 import { Land } from './land.model';
 
 @Controller('land')
 export class LandController extends ConvectorController<ChaincodeTx> {
+  
+  // Initialize mock data
   @Invokable()
-  public async create(
-    @Param(Land)
-    land: Land
-  ) {
+  public async init() {
+    let land1: Land = new Land({
+      id: '0000000',
+      parent_land_id: 'nil',
+      extent: 10000,
+      rlregistry: 'col_6',
+      current_owner_nic: '123456789V',
+      requested_new_owner_nic: 'nil',
+      boundaries: [[0,0],[0,100],[100,100],[100,0]],
+      surveyor_vote: 'S001',
+      notary_vote: 'nil',
+      current_owner_vote: 'nil'
+    });
+    let land2: Land = new Land({
+      id: '0000001',
+      parent_land_id: 'nil',
+      extent: 10000,
+      rlregistry: 'col_6',
+      current_owner_nic: '123456788V',
+      requested_new_owner_nic: 'nil',
+      boundaries: [[0,100],[0,200],[100,200],[100,100]],
+      surveyor_vote: 'S002',
+      notary_vote: 'nil',
+      current_owner_vote: 'nil'
+    });
+    let land3: Land = new Land({
+      id: '0000002',
+      parent_land_id: 'nil',
+      extent: 20000,
+      rlregistry: 'col_7',
+      current_owner_nic: '123456787V',
+      requested_new_owner_nic: 'nil',
+      boundaries: [[0,200],[0,300],[100,300],[100,200]],
+      surveyor_vote: 'S003',
+      notary_vote: 'nil',
+      current_owner_vote: 'nil'
+    });
+    let land4: Land = new Land({
+      id: '0000003',
+      parent_land_id: 'nil',
+      extent: 10000,
+      rlregistry: 'col_7',
+      current_owner_nic: '123456786V',
+      requested_new_owner_nic: 'nil',
+      boundaries: [[100,0],[100,100],[200,100],[200,0]],
+      surveyor_vote: 'S003',
+      notary_vote: 'nil',
+      current_owner_vote: 'nil'
+    });
+    let land5: Land = new Land({
+      id: '0000004',
+      parent_land_id: 'nil',
+      extent: 10000,
+      rlregistry: 'col_7',
+      current_owner_nic: '123456785V',
+      requested_new_owner_nic: 'nil',
+      boundaries: [[100,100],[100,200],[200,200],[200,100]],
+      surveyor_vote: 'S004',
+      notary_vote: 'nil',
+      current_owner_vote: 'nil'
+    });
+    let land6: Land = new Land({
+      id: '0000005',
+      parent_land_id: 'nil',
+      extent: 30000,
+      rlregistry: 'col_8',
+      current_owner_nic: '123456784V',
+      requested_new_owner_nic: 'nil',
+      boundaries: [[200,0],[200,300],[300,300],[300,0]],
+      surveyor_vote: 'S004',
+      notary_vote: 'nil',
+      current_owner_vote: 'nil'
+    });
+
+    let mockData = [land1, land2, land3, land4, land5, land6];
+    await Promise.all(mockData.map(land => land.save()));
+    return "mock data successfully initialized";
+  }
+
+  // Get land information by ID
+  @Invokable()
+  public async queryLand(@Param(yup.string()) id: string): Promise<Land> {
+    return Land.getOne(id);
+  }
+
+  // Get all lands information
+  @Invokable()
+  public async queryAllLands(): Promise<Land[]> {
+    return Land.getAll();
+  }
+
+  // Create land - for internal use only
+  @Invokable()
+  private async createLand(@Param(Land) land: Land) {
     await land.save();
+    return "successfully land created";
+  }
+
+  // Change land ownership
+  @Invokable()
+  public async changeLandOwner(@Param(yup.string()) id: string) {
+    if(this.tx.identity.getMSPID() === 'org3MSP') {
+      let land = await Land.getOne(id);
+      if (land.surveyor_vote != 'nil' && land.notary_vote != 'nil' && land.current_owner_vote != 'nil') {
+        throw new Error("Transaction not permitted by all the required parties yet.");
+      }
+      land.current_owner_nic = land.requested_new_owner_nic;
+      land.requested_new_owner_nic = 'nil';
+      land.notary_vote = 'nil';
+      land.current_owner_vote = 'nil';
+      await land.save();
+      return "Successfully changed the land ownership";
+    } else {
+      throw new Error("Authorization failed");
+    }
+  }
+
+  // Voting by surveyor
+  @Invokable()
+  public async voteSurveyor(
+    @Param(yup.string()) id: string, 
+    @Param(yup.string()) surveyorId: string) {
+    if(this.tx.identity.getMSPID() === 'org1MSP') {
+      let land = await Land.getOne(id);
+      land.surveyor_vote = surveyorId;
+      await land.save();
+      return "Surveyor vote succesfully submitted";
+    } else {
+      throw new Error("Authorization failed");
+    }
+  }
+
+  // Voting by notary
+  @Invokable()
+  public async voteNotary(
+    @Param(yup.string()) id: string,
+    @Param(yup.string()) notaryId: string,
+    @Param(yup.string()) newOwnerNIC: string) {
+    if(this.tx.identity.getMSPID() === 'org2MSP') {
+      let land = await Land.getOne(id);
+      land.notary_vote = notaryId;
+      land.requested_new_owner_nic = newOwnerNIC;
+      await land.save();
+      return "Notary vote succesfully submitted";
+    } else {
+      throw new Error("Authorization failed");
+    }
+  }
+
+  // Voting by current owner
+  @Invokable()
+  public async voteCurrentOwner(
+    @Param(yup.string()) id: string, 
+    @Param(yup.string()) ownerVote: string) {
+    if(this.tx.identity.getMSPID() === 'org3MSP') {
+      let land = await Land.getOne(id);
+      land.current_owner_vote = ownerVote;
+      await land.save();
+      return "Current owner vote succesfully submitted";
+    } else {
+      throw new Error("Authorization failed");
+    }
+  }
+
+  // Get the history records for a land by ID
+  @Invokable()
+  public async getHistoryForLand(@Param(yup.string()) id: string): Promise<History<Land>[]> {
+    if(this.tx.identity.getMSPID() === 'org2MSP' || this.tx.identity.getMSPID() === 'org3MSP') {
+      let land = await Land.getOne(id);
+      return land.history();
+    } else {
+      throw new Error("Authorization failed");
+    }
   }
 }
--- packages/land-cc/src/land.model.ts
+++ packages/land-cc/src/land.model.ts
@@ -10,18 +10,54 @@ import {
 export class Land extends ConvectorModel<Land> {
   @ReadOnly()
   @Required()
-  public readonly type = 'io.worldsibu.land';
+  public readonly type = 'io.lrbsl.land';
 
+  // ID attribute is already defined
+
+  // Parent land 
   @Required()
   @Validate(yup.string())
-  public name: string;
+  public parent_land_id: string;
 
-  @ReadOnly()
+  // Extent
   @Required()
   @Validate(yup.number())
-  public created: number;
+  public extent: number;
 
+  // RL Registry
   @Required()
-  @Validate(yup.number())
-  public modified: number;
+  @Validate(yup.string())
+  public rlregistry: string;
+
+  // Current owner NIC
+  @Required()
+  @Validate(yup.string())
+  public current_owner_nic: string;
+
+  // Requested new owner NIC
+  @Validate(yup.string())
+  public requested_new_owner_nic: string;
+
+  // Land boundaries
+  @Required()
+  @Validate(yup.array().of(yup.array().of(yup.number())))
+  public boundaries: number[][];
+
+  // Votes for transaction
+
+  // Surveyor vote
+  @Required()
+  @Validate(yup.string())
+  public surveyor_vote: string;
+
+  // Notary vote
+  @Required()
+  @Validate(yup.string())
+  public notary_vote: string;
+
+  // Current owner vote
+  @Required()
+  @Validate(yup.string())
+  public current_owner_vote: string;
+
 }
