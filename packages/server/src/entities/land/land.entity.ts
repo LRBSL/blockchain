ed2a26bf3fbb93838f86370c6a6ee6db3fd41493
RavinduSachintha <ravindusachintha53@gmail.com>
2020-02-17 07:34:21 +0000

    unused entities and services deleted

--- packages/server/src/entities/land/land.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, Entity, OneToOne, JoinColumn } from 'typeorm';
-import { Deed } from './deed.entity';
-
-// @Entity('land', { orderBy: { id: 'ASC' } })
-export class Land {
-    @Column({ type: 'varchar', primary: true })
-    id: string;
-}
